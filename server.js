c5ec9fab667172816494dd5020341b9aca69dc56
Monica <cmonikam@yahoo.es>
2021-02-02 11:32:40 -0300

    creación de contador de visitas

---
+++ server.js
@@ -0,0 +1,32 @@
+//usar express
+const express = require("express");
+const app = express();
+const port = 8000;
+
+//usar sesiones
+const session = require('express-session');
+app.use(session({secret: 'miclave'})); 
+
+//usar carpet static
+app.use(express.static(__dirname + "/static"));
+
+//usar vistas
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+
+//usar sesiones
+app.get('/', function (req, res){
+
+  if(req.session.count==undefined){
+    req.session.count=0;
+  };
+
+   req.session.count++;
+  
+  return res.render('contador',{count:req.session.count});
+  
+});
+
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
